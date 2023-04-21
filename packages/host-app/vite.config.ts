f9b2399c827881612d0bed3b850d266aaeb3d2ca
Alex Vizcaino <avizcainoluna@gmail.com>
2023-04-21 14:59:15 +0200

    fix: solves issue with base in imports from microfrontends indexes

--- packages/host-app/vite.config.ts
+++ packages/host-app/vite.config.ts
@@ -1,6 +1,24 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// const proxy = httpProxy.createProxyServer();
+
+// const proxyServer = (): PluginOption => ({
+//   name: "configure-server",
+//   apply: "serve",
+//   configureServer(server) {
+//     server.middlewares.use((req, res, next) => {
+//       next();
+//       // if (req.url && req.url.indexOf("/vue-app") > -1) {
+//       //   req.url = req.url.replace("/vue-app", "");
+//       //   proxy.web(req, res, { target: "http://localhost:8003" });
+//       // }
+//       // //req.url = req.url.replace("/vue-app", "");
+//       // else next();
+//     });
+//   },
+// });
+
 export default defineConfig({
   server: {
     host: process.env.VITE_HOST,
